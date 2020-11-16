1ebe66ef75941f360d3fb8e9a3e06972bda7e9cd
Anastasiia Ledney <72972282+AnastasiiaLedney@users.noreply.github.com>
2020-11-17 00:43:55 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="groupVue"  text-align: center>
+ <h2 class="StudentName">{{group.StudentName}}</h2>
+ <p class="Speciality">Спеціальність: {{group.Speciality}}</p>
+  <p class="Course">{{group.Course}} курс</p>
+  <p class="Variant">{{group.Variant}} варіант</p>
+
+ </div>
+
+
+</template>
+
+<script>
+export default
+{
+  name:"App",
+  data()
+  { return{
+    group:{
+      StudentName:"Ледней Анастасія Михайлівна",
+      Speciality:"Прикладна математика",
+      Course: 4,
+      Variant:2
+    }
+  }
+  }
+
+}
+</script>
+
+<style scoped>
+.StudentName{
+  font-family:"Courier New";
+     font-size: 30px;
+     color:  #F7F1CB;
+  
+}
+.Speciality{
+  font-family: "Courier New";
+     font-size: 20px;
+     color:  #F7F1CB;
+}
+.groupVue{
+position: absolute;
+border-radius: 10px 60px 30px;
+width: 400px;
+height: 300px;
+position: relative;
+background: #34568B;
+text-align: center;
+ margin: auto;
+
+}
+.Course{
+ font-family: "Courier New";
+     font-size: 20px;
+     color:  #F7F1CB;
+}
+.Variant{
+  font-family: "Courier New";
+     font-size: 20px;
+    color: #F7F1CB;
+}
+
+
+ 
+</style>
